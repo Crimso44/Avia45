629b686acf17068862ab059a6ae33ad51235ca4f
Sergey Ostrozhinsky <sergey_44@mail.ru>
2019-12-02 08:43:46 +0300

    Grid sources

---
+++ Avia45/SourceGrid4/SourceGrid/SourceGrid/Selection/CrimsoFreeSelection.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace SourceGrid.Selection
+{
+    public class CrimsoFreeSelection : FreeSelection
+    {
+        public override bool CanReceiveFocus(Position position)
+        {
+            if (Control.ModifierKeys == Keys.Control) return false;
+
+            return base.CanReceiveFocus(position);
+        }
+
+    }
+}
